0eda1e57cd05e420095d728f9cba27ed48139925
sharpsone <Sharpsone@hotmail.com>
2026-07-21 05:20:10 +0000

    force db.js ipv4

--- db.js
+++ db.js
@@ -4,7 +4,11 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
-  }
+  },
+  // Force IPv4 instead of IPv6
+  connectionTimeoutMillis: 5000,
+  keepAlive: true,
+  host: process.env.DATABASE_URL.split('@')[1].split(':')[0]
 });
 
 module.exports = {
