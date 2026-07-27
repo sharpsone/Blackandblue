75fc2077e5fc9f3b38544efe928e4be686be208c
sharpsone <Sharpsone@hotmail.com>
2026-07-27 00:28:43 +0000

    fix standings.js

--- api/standings.js
+++ api/standings.js
@@ -2,25 +2,21 @@ import { fetch, getYear, buildAuthHeaders, detectMFLHost } from "./_utils.js";
 
 export default async function handler(req, res) {
   try {
-    // Log what the browser actually sent to your backend
     console.log("REQ COOKIES HEADER:", req.headers.cookie);
 
-    // Build the Cookie header that will be forwarded to MFL
     const headers = buildAuthHeaders(req);
     console.log("AUTH HEADERS SENT TO MFL:", headers);
 
     const { leagueId } = req.query;
     const year = getYear(req);
 
+    // IMPORTANT: pass req into detectMFLHost
     const host = await detectMFLHost(year, leagueId, req);
 
     const url = `https://${host}/${year}/export?TYPE=standings&L=${leagueId}&JSON=1`;
     console.log("STANDINGS URL:", url);
 
-    const response = await fetch(url, {
-      headers
-    });
-
+    const response = await fetch(url, { headers });
     const data = await response.json();
 
     return res.json(data);
