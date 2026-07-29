d6333062104adf800d3a61e74c0d4cfc860df9ee
sharpsone <Sharpsone@hotmail.com>
2026-07-29 23:59:59 +0000

    fix a

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
-import { useContext } from "react";
-import { AuthProvider, AuthContext } from "./context/AuthContext";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
 
 import Login from "./components/Login";
-import SubmitLineup from "./pages/SubmitLineup";
+import AfterLogin from "./pages/after-login";
 import Roster from "./pages/Roster";
+import SubmitLineup from "./pages/SubmitLineup";
 import Standings from "./pages/Standings";
 
 function MainApp() {
-  const { isLoggedIn } = useContext(AuthContext);
-
-  if (!isLoggedIn) {
-    return <Login />;
-  }
-
   return (
-    <div>
-      <h1>Stinky Beavers Dashboard</h1>
-
-      <Standings />
-      <Roster />
-      <SubmitLineup />
-    </div>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/after-login" element={<AfterLogin />} />
+      <Route path="/standings" element={<Standings />} />
+      <Route path="/roster" element={<Roster />} />
+      <Route path="/submit-lineup" element={<SubmitLineup />} />
+    </Routes>
   );
 }
 
 export default function App() {
   return (
     <AuthProvider>
-      <MainApp />
+      <BrowserRouter>
+        <MainApp />
+      </BrowserRouter>
     </AuthProvider>
   );
 }
