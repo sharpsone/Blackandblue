975cbb78452e909e2efa1523082a3620ffb26cfd
sharpsone <Sharpsone@hotmail.com>
2026-07-30 00:53:21 +0000

    fix authcontext import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./components/context/AuthContext";
+import { AuthProvider } from "./context/AuthContext";
 
 import Login from "./components/Login";
 import AfterLogin from "./pages/after-login";
