4fd0eebf6d76a2cbf16d489e11f4f662e0a477c2
sharpsone <Sharpsone@hotmail.com>
2026-07-30 00:08:47 +0000

    add router to vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider } from "./components/context/AuthContext";
 
 import Login from "./components/Login";
 import AfterLogin from "./pages/after-login";
